a6aaaf10b06c64e682f6e4a26c98483f0b70f54b
jic <ji_camargo@yahoo.com>
2023-12-21 21:54:09 -0500

    add initializer\simple_form_tailwind.rb

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
+    "./app/views/**/*.turbostream.erb",
     './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    './app/javascript/**/*.js',
+    "./config/initializers/simple_form_tailwind.rb",
   ]
 }
