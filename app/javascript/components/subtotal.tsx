9a3f80339ad55363d88e709f66c2855dd9626a13
jic <ji_camargo@yahoo.com>
2024-05-01 21:27:09 -0500

    change borders on elements

--- app/javascript/components/subtotal.tsx
+++ app/javascript/components/subtotal.tsx
@@ -3,15 +3,15 @@ import styled from "styled-components"
 import { useAppDispatch, useAppSelector } from "../contexts/venue_context"
 
 const Header = styled.div`
-  font-size: 1.5rem;
+  font-size: 1.2rem;
   font-weight: bold;
   margin-left: 15px;
   margin-right: 15px;
 `
 
 const buttonClass =
-  "px-5 py-4 m-2 my-4 w-40 text-center text-white transition-colors " +
-  "duration-150 bg-gray-800 rounded-lg focus:shadow-outline hover:bg-black"
+  "px-5 py-4 m-2 my-4 w-40 text-center text-white transition-colors cursor-pointer " +
+  "duration-150 bg-gray-600 rounded-lg focus:shadow-outline hover:bg-gray-900"
 
 const Subtotal = (): React.ReactElement => {
   const myTickets = useAppSelector((state) => state.myTickets)
@@ -21,19 +21,21 @@ const Subtotal = (): React.ReactElement => {
     dispatch({ type: "clearHolds" })
   }
   return (
-    <>
-      <Header>
-        <span>Current Tickets Purchased: &nbsp;</span>
-        <span>{myTickets.length}</span>
-      </Header>
-      <Header>
-        <span>Current Tickets Cost: &nbsp;</span>
-        <span>${myTickets.length * 15}.00</span>
-      </Header>
+    <div className="flex border border-black rounded-lg w-4/5">
+      <div className="flex flex-col justify-center mr-4">
+        <Header>
+          <span>Current Tickets Purchased: &nbsp;</span>
+          <span>{myTickets.length}</span>
+        </Header>
+        <Header>
+          <span>Current Tickets Cost: &nbsp;</span>
+          <span>${myTickets.length * 15}.00</span>
+        </Header>
+      </div>
       <div className={buttonClass} onClick={onClear}>
         Clear Tickets
       </div>
-    </>
+    </div>
   )
 }
 
